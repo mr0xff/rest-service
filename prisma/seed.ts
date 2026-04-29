b0952793d8f8e382f97324b93c4c64c9c116e502
0xff <worktrabalho633@gmail.com>
2026-04-29 14:29:27 +0100

    fix: filepath

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { prisma } from "../dist/lib/prisma";
 import MessageService from "../dist/services/message.service";
 
-const prisma = new PrismaClient()
 const message = new MessageService(prisma);
 
 await message.createUserWithAction({
